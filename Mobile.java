691af9b562cb3cda7b879f227f199d0446af3297
yogananthan17 <132690377+yogananthan17@users.noreply.github.com>
2023-05-08 22:18:45 +0530

    Add files via upload
    week1.day2 TwoWheeler and Mobile assigment attached

---
+++ Mobile.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName="Vivo";
+		System.out.println(mobileBrandName);
+		char mobileLogo ='L';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece =5;
+		System.out.println(noOfMobilePiece);
+		int modelNumber =321;
+		System.out.println(modelNumber);
+		long mobileemiNumber =9876543212L;
+		System.out.println(mobileemiNumber);
+		float mobilePrice=32000.5F;
+		System.out.println(mobilePrice);
+		boolean isDamaged = false;
+		System.out.println(isDamaged);
+				
+				
+		
+	}
+
+}
+
+
+
---
+++ TwoWheeler.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 4;
+		System.out.println(noOfWheels);
+		short noofMirrors = 2;
+		System.out.println(noofMirrors);
+		long chassisNumber =987654321L;
+		System.out.println(chassisNumber);
+		boolean isPunctured =true;
+		System.out.println(isPunctured);
+		String bikeName ="pulser";
+		System.out.println(bikeName);
+		double runningKm =34.5;
+		System.out.println(runningKm);
+		
+	}
+
+
+
+			
+
+}
+
+
+
